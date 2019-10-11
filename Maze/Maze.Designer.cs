4880cc88d182ceb2e7f19298cd38acdc57cc1aac
Mujahid Islam Peal <mujahid.islam_peal@outlook.com>
2019-10-11 19:56:04 +0200

    First Commit

---
+++ Maze/Maze.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Maze
+{
+    partial class Maze
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MazePBX = new System.Windows.Forms.PictureBox();
+            this.Entry2BTN = new System.Windows.Forms.Button();
+            this.Entry1BTN = new System.Windows.Forms.Button();
+            this.Entry4BTN = new System.Windows.Forms.Button();
+            this.Entry3BTN = new System.Windows.Forms.Button();
+            this.ClickTMR = new System.Windows.Forms.Timer(this.components);
+            this.TimerLBL = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MazePBX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MazePBX
+            // 
+            this.MazePBX.BackColor = System.Drawing.Color.Transparent;
+            this.MazePBX.Location = new System.Drawing.Point(32, 29);
+            this.MazePBX.Name = "MazePBX";
+            this.MazePBX.Size = new System.Drawing.Size(720, 705);
+            this.MazePBX.TabIndex = 0;
+            this.MazePBX.TabStop = false;
+            // 
+            // Entry2BTN
+            // 
+            this.Entry2BTN.Location = new System.Drawing.Point(12, 726);
+            this.Entry2BTN.Name = "Entry2BTN";
+            this.Entry2BTN.Size = new System.Drawing.Size(75, 23);
+            this.Entry2BTN.TabIndex = 1;
+            this.Entry2BTN.Text = "Entry 2";
+            this.Entry2BTN.UseVisualStyleBackColor = true;
+            this.Entry2BTN.Click += new System.EventHandler(this.Entry2BTN_Click);
+            // 
+            // Entry1BTN
+            // 
+            this.Entry1BTN.Location = new System.Drawing.Point(12, 12);
+            this.Entry1BTN.Name = "Entry1BTN";
+            this.Entry1BTN.Size = new System.Drawing.Size(75, 23);
+            this.Entry1BTN.TabIndex = 2;
+            this.Entry1BTN.Text = "Entry 1";
+            this.Entry1BTN.UseVisualStyleBackColor = true;
+            this.Entry1BTN.Click += new System.EventHandler(this.Entry1BTN_Click);
+            // 
+            // Entry4BTN
+            // 
+            this.Entry4BTN.Location = new System.Drawing.Point(697, 12);
+            this.Entry4BTN.Name = "Entry4BTN";
+            this.Entry4BTN.Size = new System.Drawing.Size(75, 23);
+            this.Entry4BTN.TabIndex = 3;
+            this.Entry4BTN.Text = "Entry 4";
+            this.Entry4BTN.UseVisualStyleBackColor = true;
+            this.Entry4BTN.Click += new System.EventHandler(this.Entry4BTN_Click);
+            // 
+            // Entry3BTN
+            // 
+            this.Entry3BTN.Location = new System.Drawing.Point(697, 726);
+            this.Entry3BTN.Name = "Entry3BTN";
+            this.Entry3BTN.Size = new System.Drawing.Size(75, 23);
+            this.Entry3BTN.TabIndex = 4;
+            this.Entry3BTN.Text = "Entry 3";
+            this.Entry3BTN.UseVisualStyleBackColor = true;
+            this.Entry3BTN.Click += new System.EventHandler(this.Entry3BTN_Click);
+            // 
+            // ClickTMR
+            // 
+            this.ClickTMR.Tick += new System.EventHandler(this.ClickTMR_Tick);
+            // 
+            // TimerLBL
+            // 
+            this.TimerLBL.AutoSize = true;
+            this.TimerLBL.Location = new System.Drawing.Point(363, 17);
+            this.TimerLBL.Name = "TimerLBL";
+            this.TimerLBL.Size = new System.Drawing.Size(0, 13);
+            this.TimerLBL.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(360, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Timer";
+            // 
+            // Maze
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Maze.Properties.Resources.jack_square;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TimerLBL);
+            this.Controls.Add(this.Entry3BTN);
+            this.Controls.Add(this.Entry4BTN);
+            this.Controls.Add(this.Entry1BTN);
+            this.Controls.Add(this.Entry2BTN);
+            this.Controls.Add(this.MazePBX);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(800, 800);
+            this.MinimumSize = new System.Drawing.Size(800, 800);
+            this.Name = "Maze";
+            this.Text = "Maze Game";
+            this.Load += new System.EventHandler(this.Maze_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Maze_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.MazePBX)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MazePBX;
+        private System.Windows.Forms.Button Entry2BTN;
+        private System.Windows.Forms.Button Entry1BTN;
+        private System.Windows.Forms.Button Entry4BTN;
+        private System.Windows.Forms.Button Entry3BTN;
+        private System.Windows.Forms.Timer ClickTMR;
+        private System.Windows.Forms.Label TimerLBL;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Maze/Maze.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maze
+{
+    public partial class Maze : Form
+    {
+        private int i = 0;
+        
+        
+        //Ball Height
+        private int _ballHeight = 15;
+        //Ball Width
+        private int _ballWidth = 15;
+        
+        //Ball Position
+        private int _ballX=385;
+        private int _ballY=375;
+
+        //Ball Velocity
+
+        private int _ballVX=0;
+        private int _ballVY=0;
+
+        public Stopwatch Watch { get; set; }
+        
+        public Maze()
+        {
+            InitializeComponent();
+
+
+
+            Watch = new Stopwatch();
+            Watch.Start();
+        }
+
+        private void Entry1BTN_Click(object sender, EventArgs e)
+        {
+            Form.ActiveForm.Refresh();
+            ClickTMR.Start();
+            ClickTMR.Interval = 200;
+            
+            Watch.Stop();
+            TimerLBL.Text = Watch.Elapsed.TotalSeconds.ToString();
+
+            var _finalBallX = _ballX;
+            var _finalBallY = _ballY;
+
+
+            _ballX = 64;
+            _ballY = 65;
+
+            _ballVX = 5;
+            _ballVY = 5;
+
+            this.SetStyle(
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint |
+                ControlStyles.DoubleBuffer,
+                true);
+            this.UpdateStyles();
+        }
+
+        private void Entry2BTN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Entry3BTN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Entry4BTN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Maze_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void ClickTMR_Tick(object sender, EventArgs e)
+        {
+            string minutes = Watch.Elapsed.Minutes.ToString();
+            string seconds = Watch.Elapsed.Seconds.ToString();
+            TimerLBL.Text = minutes+":"+seconds;
+
+            
+            MoveBall1(++i);
+        }
+
+        private void MoveBall1(int step)
+        {
+            #region Steps for Moving the Ball to Center
+
+            if (step <= 11)
+            {
+                MoveYBall();
+            }
+            else if (step>=12 && step <=17)
+            {
+                MoveXBall();
+            }
+            else if (step>=18 && step <= 28)
+            {
+                MoveYBall();
+            }
+            else if (step >= 29 && step <= 34)
+            {
+                MoveXBall();
+            }
+            else if (step >= 35 && step <= 45)
+            {
+                MoveInverseYBall();
+            }
+            else if (step >= 46 && step <= 51)
+            {
+                MoveXBall();
+            }
+            else if (step >= 52 && step <= 67)
+            {
+                MoveYBall();
+            }
+            else if (step >= 68 && step <= 85)
+            {
+                MoveInverseXBall();
+            }
+            else if (step >= 86 && step <= 91)
+            {
+                MoveYBall();
+            }
+            else if (step >= 92 && step <= 97)
+            {
+                MoveXBall();
+            }
+            else if (step >= 98 && step <= 103)
+            {
+                MoveYBall();
+            }
+            else if (step >= 104 && step <= 109)
+            {
+                MoveInverseXBall();
+            }
+            else if (step >= 110 && step <= 137)
+            {
+                MoveYBall();
+            }
+            else if (step >= 138 && step <= 149)
+            {
+                MoveXBall();
+            }
+            else if (step >= 150 && step <= 166)
+            {
+                MoveInverseYBall();
+            }
+            else if (step >= 167 && step <= 172)
+            {
+                MoveXBall();
+            }
+            else if (step >= 173 && step <= 184)
+            {
+                MoveYBall();
+            }
+            else if (step >= 185 && step <= 195)
+            {
+                MoveXBall();
+            }
+            else if (step >= 196 && step <= 206)
+            {
+                MoveYBall();
+            }
+            else if (step >= 207 && step <= 212)
+            {
+                MoveXBall();
+            }
+            else if (step >= 213 && step <= 235)
+            {
+                MoveInverseYBall();
+            }
+            else if (step >= 236 && step <= 246)
+            {
+                MoveInverseXBall();
+            }
+            else if (step >= 247 && step <= 251)
+            {
+                MoveInverseYBall();
+            }
+            else if (step >= 252 && step <= 268)
+            {
+                MoveXBall();
+            }
+            else if (step >= 269 && step <= 279)
+            {
+                MoveYBall();
+            }
+            else if (step >= 279 && step <= 285)
+            {
+                MoveXBall();
+            }
+            else if (step >= 286 && step <= 296)
+            {
+                MoveInverseYBall();
+            }
+            else if (step >= 297 && step <= 313)
+            {
+                MoveXBall();
+            }
+            else if (step >= 314 && step <= 323)
+            {
+                if (step==323)
+                {
+                    _ballHeight =120;
+                    _ballWidth =120;
+                    _ballX = _ballX-53;
+                    _ballY = _ballY-23;
+                }
+                MoveYBall();
+            }
+
+            #endregion
+        }
+
+        private void MoveXBall()
+        {
+            _ballX += _ballVX;
+            if (_ballX < 0)
+            {
+                _ballVX = -_ballVX;
+            }
+            else if (_ballX + _ballWidth > MazePBX.Width)
+            {
+                _ballVX = -_ballVX;
+            }
+            Refresh();
+        }
+
+        private void MoveYBall()
+        {
+            _ballY += _ballVY;
+            if (_ballY < 0)
+            {
+                _ballVX = -_ballVY;
+            }
+            else if (_ballY + _ballHeight > MazePBX.Height)
+            {
+                _ballVY = -_ballVY;
+            }
+
+            Refresh();
+        }
+
+
+        private void MoveInverseXBall()
+        {
+            _ballX -= _ballVX;
+            if (_ballX < 0)
+            {
+                _ballVX = -_ballVX;
+            }
+            else if (_ballX + _ballWidth > MazePBX.Width)
+            {
+                _ballVX = -_ballVX;
+            }
+            Refresh();
+        }
+
+        private void MoveInverseYBall()
+        {
+            _ballY -= _ballVY;
+            if (_ballY < 0)
+            {
+                _ballVX = -_ballVY;
+            }
+            else if (_ballY + _ballHeight > MazePBX.Height)
+            {
+                _ballVY = -_ballVY;
+            }
+
+            Refresh();
+        }
+
+        private void Maze_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.FillEllipse(Brushes.Blue, _ballX, _ballY,
+                _ballWidth, _ballHeight);
+            e.Graphics.DrawEllipse(Pens.Black, _ballX, _ballY,
+                _ballWidth, _ballHeight);
+        }
+    }
+}
